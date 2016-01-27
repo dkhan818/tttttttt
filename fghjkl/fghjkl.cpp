0575bc1257727b4162f5de58b9c99b2063c119b8
han <han@han-PC>
2016-01-28 07:55:30 +0900

    4-1

--- fghjkl/fghjkl.cpp
+++ fghjkl/fghjkl.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!1234" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!1234-1" << endl; // prints !!!Hello World!!!
 	return 0;
 }
