e69bba1ffc847c4e42c7d0f82a5e0ca4ca906e41
hanpc <hanpc@DESKTOP-VADPRE2>
2016-01-28 18:40:38 +0900

    asdfghj

--- fghjkl/fghjkl.cpp
+++ fghjkl/fghjkl.cpp
@@ -16,7 +16,7 @@ int main() {
 <<<<<<< HEAD
 	cout << "!!!Hello World!!!1234-12345" << endl; // prints !!!Hello World!!!
 =======
-	cout << "!!!Hello World!!!1234-123" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!1234-12366666666666" << endl; // prints !!!Hello World!!!
 >>>>>>> refs/heads/master
 
 	return 0;
