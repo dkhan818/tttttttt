7c628a1ab0fd4522848f4ddf0035145be3d97ffe
hanpc <hanpc@DESKTOP-VADPRE2>
2016-01-28 14:44:25 +0900

    hnjm,

--- fghjkl/fghjkl.cpp
+++ fghjkl/fghjkl.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!1234-1" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!1234-13" << endl; // prints !!!Hello World!!!
 	return 0;
 }
